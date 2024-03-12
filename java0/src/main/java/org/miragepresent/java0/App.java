327c89afd962bf54afb8d1240d8ec70ba4cb9fc2
Davyd Holovii <mirage.present@gmail.com>
2024-03-11 22:41:05 -0400

    Configurable server port

--- java0/src/main/java/org/miragepresent/java0/App.java
+++ java0/src/main/java/org/miragepresent/java0/App.java
@@ -1,6 +1,7 @@
 package org.miragepresent.java0;
 
 import java.net.InetSocketAddress;
+import java.rmi.ServerException;
 
 import org.miragepresent.java0.controller.HomePageHandler;
 
@@ -15,12 +16,23 @@ public class App
     public static void main( String[] args )
     {
         try { 
-            Long serverPort = 8080L;
-            System.out.println("Start listening on port " + serverPort + "...");
-            HttpServer server = HttpServer.create(new InetSocketAddress(serverPort.intValue()),0);
-            server.createContext("/", new HomePageHandler());
-            server.setExecutor(null);
-            server.start();
+            String serverPort = System.getenv("HTTP_LISTENING_PORT");
+            
+            if (serverPort == null) {
+                System.out.println("HTTP_LISTENING_PORT is not define. Defaulting to 8080");
+                serverPort = "8080";
+            }
+
+            try {
+                Long portNumber = Long.valueOf(serverPort);
+                System.out.println("Start listening on port " + serverPort + "...");
+                HttpServer server = HttpServer.create(new InetSocketAddress(portNumber.intValue()),0);
+                server.createContext("/", new HomePageHandler());
+                server.setExecutor(null);
+                server.start();
+            } catch (NumberFormatException e) {
+                throw new ServerException("Cannot start webserver. HTTP_LISTENING_PORT is invalid");
+            }
         } catch (Exception e) {
             System.out.print("Server error: " + e.getMessage());
         }
