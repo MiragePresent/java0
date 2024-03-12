415fa900c5af073924024d686918875bcb458f2a
Davyd Holovii <mirage.present@gmail.com>
2024-03-11 22:28:18 -0400

    Adds http server

--- java0/src/main/java/org/miragepresent/java0/App.java
+++ java0/src/main/java/org/miragepresent/java0/App.java
@@ -1,5 +1,11 @@
 package org.miragepresent.java0;
 
+import java.net.InetSocketAddress;
+
+import org.miragepresent.java0.controller.HomePageHandler;
+
+import com.sun.net.httpserver.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try { 
+            Long serverPort = 8080L;
+            System.out.println("Start listening on port " + serverPort + "...");
+            HttpServer server = HttpServer.create(new InetSocketAddress(serverPort.intValue()),0);
+            server.createContext("/", new HomePageHandler());
+            server.setExecutor(null);
+            server.start();
+        } catch (Exception e) {
+            System.out.print("Server error: " + e.getMessage());
+        }
     }
 }
